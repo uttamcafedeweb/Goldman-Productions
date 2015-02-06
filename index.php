d95fabb6bd4fb41b1e927b0def497f1c0aab31a1
uttamcafedeweb <uttam.cafedeweb@gmail.com>
2015-02-06 22:31:55 +0545

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Goldman Productions</title>
+    <link href="css/custom.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="content">
+        <iframe src="http://goldmanproductions.com/" name="test" height="100%" scrolling="no" width="100% ">You need a Frames Capable browser to view this content.</iframe>
+    </div>
+  </body>
+</html>
